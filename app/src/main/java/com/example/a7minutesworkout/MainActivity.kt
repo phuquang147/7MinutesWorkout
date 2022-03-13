17df0cb529358fedd81b832100ec69b41c47abe0
phuquang147 <phuquang14722@gmail.com>
2022-03-13 14:04:19 +0700

    Set up the UI

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flButtonStart : FrameLayout = findViewById(R.id.flStart)
+        flButtonStart.setOnClickListener() {
+            Toast.makeText(this, "hello", Toast.LENGTH_LONG).show()
+        }
     }
 }
