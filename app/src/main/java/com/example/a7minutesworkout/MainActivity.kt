ffef55c6ba5b5476ec461c02cb7357a464e7fb7f
phuquang147 <phuquang14722@gmail.com>
2022-03-14 21:42:39 +0700

    Use viewbinding and get rid of actionbar

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flButtonStart : FrameLayout = findViewById(R.id.flStart)
-        flButtonStart.setOnClickListener() {
+        binding?.flStart?.setOnClickListener() {
             Toast.makeText(this, "hello", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
